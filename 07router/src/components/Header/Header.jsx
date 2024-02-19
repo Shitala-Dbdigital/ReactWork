b218aff60f3db1fced65c760184880b2736fd053
Shitala <59687806+Shitala-Dbdigital@users.noreply.github.com>
2024-02-19 19:26:23 +0530

    context api

--- 07router/src/components/Header/Header.jsx
+++ 07router/src/components/Header/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink to="/"
-                                    className={(isActive) =>
+                                    className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 ${isActive?"text-orange-700":"text-gray-700"} duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -42,7 +42,7 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/about"                                   className={(isActive) =>
+                                <NavLink to="/about"                                   className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 ${isActive?"text-orange-700":"text-gray-700"} duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -50,7 +50,7 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/contact"                                   className={(isActive) =>
+                                <NavLink to="/contact"                                   className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 ${isActive?"text-orange-700":"text-gray-700"} duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -58,7 +58,7 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/github"                                   className={(isActive) =>
+                                <NavLink to="/github"                                   className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 ${isActive?"text-orange-700":"text-gray-700"} duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
